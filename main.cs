efe3b6bfa183b8a4d2b0a60b0d715301cadb2775
Eve <84185604+evil33333333@users.noreply.github.com>
2022-02-28 00:07:57 -0800

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		  Console.WriteLine("As I have respect for the developer, I will not release the source.");
+      Console.WriteLine("Cheers again, Eve.");
+	}
+}
