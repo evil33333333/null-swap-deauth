d206cd6abcc0dd1a0a24ee3e31a618cbfed16996
Eve <84185604+evil33333333@users.noreply.github.com>
2022-02-28 00:08:24 -0800

    Update main.cs

--- main.cs
+++ main.cs
@@ -5,6 +5,6 @@ public class Program
 	public static void Main()
 	{
 		  Console.WriteLine("As I have respect for the developer, I will not release the source.");
-      Console.WriteLine("Cheers again, Eve.");
+      		  Console.WriteLine("Cheers again, Eve.");
 	}
 }
